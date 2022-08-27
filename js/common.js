6c58ddc2e5e7537370fc47853d4d5faae5e044b5
Anant Anand Gupta <anantanandgupta@gmail.com>
2022-08-27 23:05:36 +0530

    Add content

--- js/common.js
+++ js/common.js
@@ -81,9 +81,9 @@ VERSION : 1.1
       {
           $(".nim-menu").css({height:"0px",padding:"15px",background:"#000",boxShadow:"2px 2px 3px 3px rgba(0,0,0,0.3"});
           $("a.navbar-brand").css("color","#fff");          
-          $(".nim-menu.navbar-default .navbar-nav > li > a").css("color","#fafafa");
+          $(".nim-menu.navbar-default .navbar-nav > li > a").css("color","#05afbf");
           $(".nim-menu.navbar-default .navbar-nav > li > a:hover").css("color",themecolor);
-          $(".nim-menu.navbar-default .navbar-nav > li > a").hover(function(){$(this).css("color",themecolor)},function(){$(this).css("color","#fafafa")})
+          $(".nim-menu.navbar-default .navbar-nav > li > a").hover(function(){$(this).css("color",themecolor)},function(){$(this).css("color","#05afbf")})
       }
        }
        )
